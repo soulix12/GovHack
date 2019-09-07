6bd2aec28dcdea727cb6dd6e774a81691b8bb3ee
Skidmore <jeremy.skidmore@capgemini.com>
2019-09-07 20:49:25 +1000

    coupling of react and flask

--- src/flask_demo.py
+++ src/flask_demo.py
@@ -1,8 +1,8 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}'
+    message = "Hello, World"
+    return render_template('index.html', message=message)
